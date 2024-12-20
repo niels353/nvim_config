70f5fdd8ac20b4239d2630fbc4c5a304647cc3a3
niels353 <pahlniels525@gmail.com>
2024-12-20 21:54:52 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+print("test")
+print("test")
+print("test")
+
+-- test
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
