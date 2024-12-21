5fa3aceef37b4f98eb19e6f3cf60a14572039272
niels353 <pahlniels525@gmail.com>
2024-12-21 15:50:53 +0100

    added lazy and colorsheme

--- init.lua
+++ init.lua
@@ -2,9 +2,10 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+require("config.lazy")
+
 -- Highlight when yanking (copying) text
 -- See `:help vim.highlight.on_yank()`
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
