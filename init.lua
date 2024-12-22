fd01155833601f5ceba86567e440b2bd44032f1d
niels353 <pahlniels525@gmail.com>
2024-12-22 17:28:28 +0100

    added lua ls

--- init.lua
+++ init.lua
@@ -25,9 +25,9 @@ set("n", "<c-h>", "<c-w><c-h>")
 -- Highlight when yanking (copying) text
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -9,37 +9,44 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup {
-	ensure_installed = { "lua_ls" },
+        ensure_installed = { "lua_ls" },
       }
     end
   },
   {
-    "neovim/nvim-lspconfig", 
+    "neovim/nvim-lspconfig",
     dependencies = {
       'saghen/blink.cmp',
+      {
+        "folke/lazydev.nvim",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
     },
 
     config = function()
-
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       require("lspconfig").lua_ls.setup { capabilites = capabilities }
 
       vim.keymap.set("n", "<space>ff", function() vim.lsp.buf.format() end)
 
       vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(args)
-	  local client = vim.lsp.get_client_by_id(args.data.client_id)
-	  if not client then return end
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
 
-	  if client.supports_method('textDocument/formatting') then
-	    vim.api.nvim_create_autocmd('BufWritePre', {
-	      buffer = args.buf,
-	      callback = function()
-		vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-	      end,
-	    })
-	  end
-	end,
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
       })
     end,
   }
