284dd45db3738e400ad06553ad8033d2f43387c5
niels353 <pahlniels525@gmail.com>
2024-12-22 17:21:25 +0100

    added mason

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,11 +1,28 @@
 return {
   {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+	ensure_installed = { "lua_ls" },
+      }
+    end
+  },
+  {
     "neovim/nvim-lspconfig", 
     dependencies = {
       'saghen/blink.cmp',
     },
+
     config = function()
+
       local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require("lspconfig").lua_ls.setup { capabilites = capabilities }
 
       vim.keymap.set("n", "<space>ff", function() vim.lsp.buf.format() end)
 
